0866b301bf7c5e55332614f31ff65d85c72d93dd
Laurent B. <123247286+laurentbouleau@users.noreply.github.com>
2024-11-23 14:38:00 +0100

    Novembre 25 (23/11/2024)
    film.cpp

--- film.cpp
+++ film.cpp
@@ -68,20 +68,33 @@ SequenceVisionnage_film::SequenceVisionnage_film(fs::path const& m_cheminFichier
 
     // (((([[:digit:]]{4})-([[:digit:]]{2})-([[:digit:]]{2})|([[:digit:]]{2})-([[:digit:]]{2})|([[:digit:]]{2}))(_?))+)(\s(.+))?
     const std::wstring date_year_month_day_format = L"([[:digit:]]{4})-([[:digit:]]{2})-([[:digit:]]{2})";
+
     const std::wstring date_month_day_format = L"([[:digit:]]{2})-([[:digit:]]{2})";
+
     const std::wstring date_day_format = L"([[:digit:]]{2})";
+
     const std::wstring stream_format = L"(\\s(.+))?";
+
     const std::wstring dates_format = L"((" + date_year_month_day_format + L"|" + date_month_day_format + L"|" + date_day_format + L")(_?))";
+
     //const std::wstring dates_format = L"((" + date_year_month_day_format + L"|" + date_month_day_format + L"|" + date_day_format + L")" + stream_format;
 
     const int dates_full_match_index = 0;
+
     const int dates_date_year_month_day_year_index = dates_full_match_index + 3;
+
     const int dates_date_year_month_day_month_index = dates_date_year_month_day_year_index + 1;
+
     const int dates_date_year_month_day_day_index = dates_date_year_month_day_month_index + 1;
+
     const int dates_date_month_day_month_index = dates_date_year_month_day_day_index + 1;
+
     const int dates_date_month_day_day_index = dates_date_month_day_month_index + 1;
+
     const int dates_date_day_day_index = dates_date_month_day_day_index + 1;
+
     const int dates_someFlag_index = dates_date_day_day_index + 2;
+
 
     //const int dates_full_match_index_f = 0;
     //const int dates_date_year_month_day_year_index_f = dates_full_match_index + 3;
@@ -103,10 +116,10 @@ SequenceVisionnage_film::SequenceVisionnage_film(fs::path const& m_cheminFichier
     const int filename_date_month_day_day_index = filename_date_month_day_month_index + 1;
     const int filename_date_day_day_index = filename_date_month_day_day_index + 1;
     //const int filename_someFlag_index = filename_date_day_day_index + 2;
-    const int filename_someFlag_index = filename_date_day_day_index + 1;
+    const int filename_someFlag_index = filename_date_day_day_index + 2;
     //const int filename_stream_index = filename_someFlag_index + 2;
     //const int filename_stream_index = filename_someFlag_index + 3;
-    const int filename_stream_index = filename_someFlag_index + 4;
+    const int filename_stream_index = filename_someFlag_index + 3;
 
     auto nomFichier = m_cheminFichier.wstring();
 
